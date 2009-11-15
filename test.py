ffdac8b138ec3af8ac828a500536b1e60f3110ef
Roy Liu <royliu@cs.ucsd.edu>
2009-11-14 20:27:40 -0800

    Update configuration files

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ import subprocess
 import sys
 import time
 
+from subprocess import Popen
+
 def main():
     """
     The main method body.
@@ -18,14 +20,14 @@ def main():
     javacmd = ["java", "-ea", "-Xmx128M", "-cp", "dapper.jar"]
 
     processes = []
-    processes.append(subprocess.Popen(javacmd + ["dapper.ui.FlowManagerDriver", "--port", "12121", "--archive", "dapper-ex.jar", "ex.SimpleTest"]))
+    processes.append(Popen(javacmd + ["dapper.ui.FlowManagerDriver", "--port", "12121", "--archive", "dapper-ex.jar", "ex.SimpleTest"]))
 
     time.sleep(2)
 
     nclients = 4
 
     for i in range(nclients):
-        processes.append(subprocess.Popen(javacmd + ["dapper.client.ClientDriver", "--host", "localhost:12121"]))
+        processes.append(Popen(javacmd + ["dapper.client.ClientDriver", "--host", "localhost:12121"]))
 
     while sys.stdin.read(1) != "\n":
         pass
