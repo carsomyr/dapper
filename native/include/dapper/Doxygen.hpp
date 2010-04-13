4dd78a4ef5c05d448409788639faa8b4a44bf313
Roy Liu <royliu@cs.ucsd.edu>
2010-04-12 18:20:44 -0700

    Update the copyrights on various files

--- native/include/dapper/Doxygen.hpp
+++ native/include/dapper/Doxygen.hpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of Dapper, the Distributed and Parallel Program Execution Runtime ("this library").
  * 
- * Copyright (C) 2009 Roy Liu
+ * Copyright (C) 2009 The Regents of the University of California
  * 
  * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation, either version 2.1 of the License, or (at your option)
--- native/src/win32/BuildAndTest.cpp
+++ native/src/win32/BuildAndTest.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of Dapper, the Distributed and Parallel Program Execution Runtime ("this library").
  * 
- * Copyright (C) 2008 Roy Liu, The Regents of the University of California
+ * Copyright (C) 2008-2010 The Regents of the University of California
  * 
  * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation, either version 2.1 of the License, or (at your option)
